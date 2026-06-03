9f30a63d115595f199eb7bc6040dec0525794fd0
karamveergill3 <karamveergill3@gmail.com>
2026-06-03 20:34:29 +0100

    Update vite.config.js

--- mit/vite.config.js
+++ mit/vite.config.js
@@ -1,15 +1,1 @@
-import { defineConfig } from 'vite'
-
-export default defineConfig({
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: 'index.html',
-        login: 'login.html',
-        pending: 'pending.html',
-        admin: 'admin.html'
-      }
-    }
-  }
-})
+export default {}
